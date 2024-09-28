c8b87bc123157d94e895c5495b612a312c67c2d9
Kevin <jf4044@163.com>
2024-09-28 12:02:58 +0000

    new build action

---
+++ .github/scripts/build_boot_img.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+DOWNLOAD_SERVER="images.linuxcontainers.org"
+DOWNLOAD_INDEX_PATH="/meta/1.0/index-system"
+DOWNLOAD_DISTRO="debian;bookworm;arm64;default"
+
+DTB_FILE=msm8916-cj-sp970.dtb
+RAMDISK_FILE=initrd.img
+
+rootfs_url="https://$DOWNLOAD_SERVER$(curl -m 10 -fsSL "https://$DOWNLOAD_SERVER$DOWNLOAD_INDEX_PATH" | grep "$DOWNLOAD_DISTRO" | cut -f 6 -d ';')rootfs.tar.xz"
+echo "Download rootfs from $rootfs_url"
+curl -L -o rootfs.tar.xz "$rootfs_url"
+mkdir rootfs && tar -xf rootfs.tar.xz -C rootfs && rm rootfs.tar.xz
+
+cat <<EOF > rootfs/tmp/chroot.sh
+#!/bin/bash
+
+rm /etc/resolv.conf
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
+apt update
+apt install -y initramfs-tools
+apt install -y /tmp/*.deb
+
+exit
+EOF
+chmod 755 rootfs/tmp/chroot.sh
+cp ../../artifacts/linux-*.deb rootfs/tmp/
+mount --bind /proc rootfs/proc
+mount --bind /dev rootfs/dev
+mount --bind /dev/pts rootfs/dev/pts
+mount --bind /sys rootfs/sys
+LANG=C LANGUAGE=C LC_ALL=C chroot rootfs /tmp/chroot.sh
+umount rootfs/proc
+umount rootfs/dev/pts
+umount rootfs/dev
+umount rootfs/sys
+cp rootfs/boot/vmlinuz* ./Image.gz
+cp rootfs/boot/initrd.img* ./initrd.img
+cp rootfs/usr/lib/linux-image*/qcom/*sp970*.dtb ./
+
+cat Image.gz $DTB_FILE > kernel-dtb
+mkbootimg \
+    --base 0x80000000 \
+    --kernel_offset 0x00080000 \
+    --ramdisk_offset 0x02000000 \
+    --tags_offset 0x01e00000 \
+    --pagesize 2048 \
+    --second_offset 0x00f00000 \
+    --ramdisk $RAMDISK_FILE \
+    --cmdline "androidboot.hardware=qcom androidboot.oem.product=HL6180W rw"\
+    --kernel kernel-dtb -o boot.img
+
+mv boot*.img ../../artifacts/
