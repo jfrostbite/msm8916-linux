22fecfc48b210498647e47d205db02dbd931ee4f
Kevin <jf4044@163.com>
2024-10-02 10:53:21 +0800

    restore kernel

--- .github/scripts/build_boot_img.sh
+++ .github/scripts/build_boot_img.sh
@@ -47,13 +47,13 @@ cp rootfs/usr/lib/linux-image*/qcom/*sp970*.dtb ./
 cat Image.gz $DTB_FILE > kernel-dtb
 mkbootimg \
     --base 0x80000000 \
-    --kernel_offset 0x00008000 \
-    --ramdisk_offset 0x01000000 \
-    --tags_offset 0x00000100 \
+    --kernel_offset 0x00080000 \
+    --ramdisk_offset 0x02000000 \
+    --tags_offset 0x01e00000 \
     --pagesize 2048 \
     --second_offset 0x00f00000 \
     --ramdisk $RAMDISK_FILE \
-    --cmdline "earlycon root=PARTUUID=a7ab80e8-e9d1-e8cd-57f1-93f69b1d141e console=ttyMSM0,115200 no_framebuffer=true rw"\
+    --cmdline "earlycon root=PARTLABEL=rootfs console=ttyMSM0,115200 no_framebuffer=true rw"\
     --kernel kernel-dtb -o boot.img
 
 mv boot*.img ../../artifacts/
