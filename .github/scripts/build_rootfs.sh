13e8148ffa9348288b00dfbaea0092587ee71f03
Kevin <jf4044@163.com>
2024-10-02 23:01:37 +0800

    Update build_rootfs.sh

--- .github/scripts/build_rootfs.sh
+++ .github/scripts/build_rootfs.sh
@@ -6,11 +6,12 @@ mkdir debian build
 debootstrap --arch=arm64 --foreign $DIST debian https://deb.debian.org/debian/
 LANG=C LANGUAGE=C LC_ALL=C chroot debian /debootstrap/debootstrap --second-stage
 cp ../../artifacts/linux-*.deb chroot.sh debian/tmp/
+chmod +x debian/tmp/chroot.sh
 mount --bind /proc debian/proc
 mount --bind /dev debian/dev
 mount --bind /dev/pts debian/dev/pts
 mount --bind /sys debian/sys
-LANG=C LANGUAGE=C LC_ALL=C chroot debian chmod +x /tmp/chroot.sh && /tmp/chroot.sh
+LANG=C LANGUAGE=C LC_ALL=C chroot debian /tmp/chroot.sh
 umount debian/proc
 umount debian/dev/pts
 umount debian/dev
