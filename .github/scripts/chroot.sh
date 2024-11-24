c34a85afb266af6a520049f559b147b8ca6358f8
Kevin <kevin@Kevins-MBP.lan>
2024-11-24 22:54:26 +0800

    update chroot.sh

--- .github/scripts/chroot.sh
+++ .github/scripts/chroot.sh
@@ -99,7 +99,5 @@ systemctl enable mobian-startup.timer
 
 update-alternatives --set iptables /usr/sbin/iptables-legacy
 update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
-update-alternatives --set arptables /usr/sbin/arptables-legacy
-update-alternatives --set ebtables /usr/sbin/ebtables-legacy
 
 exit
