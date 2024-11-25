3b0572331d753124e675875db636a1156b1e5588
kevin <kevin@kev.cn>
2024-11-25 19:51:59 +0800

    upload script

---
+++ .github/scripts/build.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+# Constants
+DOWNLOAD_SERVER="images.linuxcontainers.org"
+DOWNLOAD_INDEX_PATH="/meta/1.0/index-system"
+DOWNLOAD_DISTRO="debian;bookworm;arm64;default"
+DIST=bookworm
+DEVICE="uz801"
+VENDOR="alk"
+DTB_FILE="msm8916-${VENDOR}-${DEVICE}.dtb"
+CMDLINE="earlycon console=tty0 console=ttyMSM0,115200 root=PARTLABEL=system rw"
+
+# 判断../../artifacts/是否存在，并且有linux-*.deb文件，如果不存在，那么从../../../中复制, 如果复制失败，那么退出
+if [ ! -d "../../artifacts/" ] || [ ! -f "../../artifacts/linux-*.deb" ]; then
+    echo "artifacts directory or linux-*.deb file not found, copying from ../../../artifacts/"
+    mkdir -p ../../artifacts
+    cp -r ../../../linux-image*.deb ../../artifacts/
+    cp -r ../../../linux-headers*.deb ../../artifacts/
+    if [ $? -ne 0 ]; then
+        echo "Failed to copy linux-*.deb from ../../../artifacts/, exiting."
+        exit 1
+    fi
+fi
+
+# 1. Download and prepare rootfs
+echo "Preparing rootfs..."
+if [ ! -d "rootfs" ]; then
+    rootfs_url="https://$DOWNLOAD_SERVER$(curl -m 10 -fsSL "https://$DOWNLOAD_SERVER$DOWNLOAD_INDEX_PATH" | grep "$DOWNLOAD_DISTRO" | cut -f 6 -d ';')rootfs.tar.xz"
+    echo "Download rootfs from $rootfs_url"
+    curl -L -o rootfs.tar.xz "$rootfs_url"
+    mkdir rootfs && tar -xf rootfs.tar.xz -C rootfs && rm rootfs.tar.xz
+else
+    echo "rootfs directory already exists, skipping download."
+fi
+
+# 2. Build boot image
+echo "Building boot image..."
+# Setup chroot environment for boot image
+cat <<EOF > rootfs/tmp/chroot.sh
+#!/bin/bash
+
+rm /etc/resolv.conf
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
+apt update
+apt install -y initramfs-tools
+apt install -y /tmp/*.deb
+
+exit
+EOF
+
+chmod 755 rootfs/tmp/chroot.sh
+cp ../../artifacts/linux-*.deb rootfs/tmp/
+
+# Mount and chroot
+mount --bind /proc rootfs/proc
+mount --bind /dev rootfs/dev
+mount --bind /dev/pts rootfs/dev/pts
+mount --bind /sys rootfs/sys
+LANG=C LANGUAGE=C LC_ALL=C chroot rootfs /tmp/chroot.sh
+
+# Unmount
+umount rootfs/proc
+umount rootfs/dev/pts
+umount rootfs/dev
+umount rootfs/sys
+
+# Copy necessary files
+cp rootfs/boot/vmlinuz* ./Image.gz
+cp rootfs/boot/initrd.img* ./initrd.img
+cp rootfs/usr/lib/linux-image*/qcom/$DTB_FILE ./
+
+echo "DTB file: $DTB_FILE"
+file $DTB_FILE
+
+# Create boot image
+cat Image.gz $DTB_FILE > kernel-dtb
+mkbootimg \
+    --base 0x80000000 \
+    --kernel_offset 0x00008000 \
+    --ramdisk_offset 0x01000000 \
+    --tags_offset 0x00000100 \
+    --pagesize 2048 \
+    --second_offset 0x00f00000 \
+    --ramdisk initrd.img \
+    --cmdline "$CMDLINE" \
+    --kernel kernel-dtb -o boot.img
+
+mv boot.img ../../artifacts/
+
+# 3. Build system rootfs
+echo "Building system rootfs..."
+# Copy necessary files
+cp -r ../../artifacts/*.deb chroot.sh firmware/ rootfs/tmp/
+chmod +x rootfs/tmp/chroot.sh
+
+# Mount necessary directories
+mount --bind /proc rootfs/proc
+mount --bind /dev rootfs/dev
+mount --bind /dev/pts rootfs/dev/pts
+mount --bind /sys rootfs/sys
+
+# Chroot and setup
+LANG=C LANGUAGE=C LC_ALL=C chroot rootfs /tmp/chroot.sh
+
+# Unmount
+umount rootfs/proc
+umount rootfs/dev/pts
+umount rootfs/dev
+umount rootfs/sys
+
+# Save debian version
+cp rootfs/etc/debian_version ../../
+
+# Cleanup
+rm -rf rootfs/tmp/* rootfs/root/.bash_history > /dev/null 2>&1
+
+# Create and process rootfs image
+dd if=/dev/zero of=debian-${DEVICE}.img bs=1M count=$(( $(du -ms rootfs | cut -f1) + 100 ))
+mkfs.ext4 -L rootfs debian-${DEVICE}.img
+mkdir -p build
+mount debian-${DEVICE}.img build
+rsync -aH rootfs/ build/
+umount build
+img2simg debian-${DEVICE}.img rootfs.img
+rm -rf debian-${DEVICE}.img build Image.gz initrd.img kernel-dtb *.dtb > /dev/null 2>&1
+# xz rootfs.img
+mv rootfs.img ../../artifacts/
+
+# Final cleanup
+# rm -rf rootfs
---
+++ .github/scripts/chroot.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+LANG_TARGET=en_US.UTF-8
+PASSWORD=adminuz801
+NAME=uz801
+
+rm /etc/resolv.conf
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+
+apt update
+apt full-upgrade -y
+apt install -y apt-transport-https ca-certificates
+apt install -y initramfs-tools locales openssh-server systemd-timesyncd fake-hwclock zram-tools rmtfs qrtr-tools dnsmasq iptables nano network-manager
+# apt install -y /tmp/openstick-utils.deb
+apt install -y /tmp/linux-image*.deb
+
+mkdir -p /lib/firmware/msm-firmware-loader
+chmod +x /tmp/firmware/msm-firmware-loader.sh
+chmod +x /tmp/firmware/mobian-usb-gadget
+chmod +x /tmp/firmware/mobian-setup-usb-network
+chmod +x /tmp/firmware/mobian-expandisk-startup.sh
+chmod +x /tmp/firmware/mobian-startup.sh
+chmod +x /tmp/firmware/gc
+chmod +x /tmp/firmware/adbd
+chmod +x /tmp/firmware/run-iptables
+chmod +x /tmp/firmware/uim-slot-selection.sh
+cp /tmp/firmware/msm-firmware-loader.sh /usr/sbin/
+cp /tmp/firmware/mobian-usb-gadget /usr/sbin/
+cp /tmp/firmware/mobian-setup-usb-network /usr/sbin/
+cp /tmp/firmware/mobian-expandisk-startup.sh /usr/sbin/
+cp /tmp/firmware/mobian-startup.sh /usr/sbin/
+cp /tmp/firmware/gc /usr/bin/
+cp /tmp/firmware/adbd /usr/bin/
+
+cp /tmp/firmware/msm-firmware-loader.service /etc/systemd/system/
+cp /tmp/firmware/mobian-usb-gadget.service /etc/systemd/system/
+cp /tmp/firmware/mobian-setup-usb-network.service /etc/systemd/system/
+cp /tmp/firmware/mobian-ssh-keygen.service /etc/systemd/system/
+cp /tmp/firmware/mobian-expandisk-startup.service /etc/systemd/system/
+cp /tmp/firmware/mobian-startup.service /etc/systemd/system/
+cp /tmp/firmware/mobian-startup.timer /etc/systemd/system/
+cp -r /tmp/firmware/qcom/ /lib/firmware/
+
+if [ ! -e /etc/dnsmasq.d ]; then
+    mkdir -p /etc/dnsmasq.d
+fi
+cp /tmp/firmware/dnsmasq.conf /etc/dnsmasq.d/
+if [ ! -e /etc/network/if-up.d ]; then
+    mkdir -p /etc/network/if-up.d
+fi
+cp /tmp/firmware/run-iptables /etc/network/if-up.d/
+cp /tmp/firmware/firewall.conf /etc/firewall.conf
+
+
+cp /tmp/firmware/uim-slot-selection.sh /usr/sbin/
+cp /tmp/firmware/uim-slot-selection.service /etc/systemd/system/
+
+sed -i -e "s/# $LANG_TARGET UTF-8/$LANG_TARGET UTF-8/" /etc/locale.gen
+dpkg-reconfigure --frontend=noninteractive locales
+update-locale LANG=$LANG_TARGET LC_ALL=$LANG_TARGET LANGUAGE=$LANG_TARGET
+
+echo -e "$PASSWORD\n$PASSWORD" | passwd
+echo $NAME > /etc/hostname
+
+sed -i 's/^.\?PermitRootLogin.*$/PermitRootLogin yes/g' /etc/ssh/sshd_config
+sed -i 's/^.\?ALGO=.*$/ALGO=zstd/g' /etc/default/zramswap
+sed -i 's/^.\?PERCENT=.*$/PERCENT=150/g' /etc/default/zramswap
+
+cat <<EOF > /etc/apt/sources.list
+deb http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+# deb-src http://deb.debian.org/debian/ bookworm main contrib non-free non-free-firmware
+
+deb http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
+# deb-src http://deb.debian.org/debian/ bookworm-updates main contrib non-free non-free-firmware
+
+deb http://deb.debian.org/debian/ bookworm-backports main contrib non-free non-free-firmware
+# deb-src http://deb.debian.org/debian/ bookworm-backports main contrib non-free non-free-firmware
+
+deb http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+# deb-src http://security.debian.org/debian-security bookworm-security main contrib non-free non-free-firmware
+EOF
+
+rm -rf /etc/ssh/ssh_host_* /var/lib/apt/lists
+rm -rf /tmp/* /root/.bash_history > /dev/null 2>&1
+rm -rf /var/lib/apt/lists
+rm -rf /etc/resolv.conf
+apt clean all
+
+sed -i 's/#DNSStubListener=yes/DNSStubListener=no/' /etc/systemd/resolved.conf
+sed -i 's/#SystemMaxUse=/SystemMaxUse=100M/' /etc/systemd/journald.conf
+sed -i 's/#SystemMaxFileSize=/SystemMaxFileSize=50M/' /etc/systemd/journald.conf
+sed -i 's/#SystemMaxFiles=100/SystemMaxFiles=10/' /etc/systemd/journald.conf
+sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/' /etc/sysctl.conf
+
+systemctl enable msm-firmware-loader
+systemctl enable mobian-usb-gadget
+systemctl enable mobian-setup-usb-network
+systemctl enable mobian-ssh-keygen
+systemctl enable mobian-expandisk-startup
+systemctl enable mobian-startup.timer
+
+update-alternatives --set iptables /usr/sbin/iptables-legacy
+update-alternatives --set ip6tables /usr/sbin/ip6tables-legacy
+
+exit
