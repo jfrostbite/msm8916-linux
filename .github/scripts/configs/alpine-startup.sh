04236742abb309597cdcfa6fef94e9bcab348add
Kevin <kevin@Kevins-MBP.lan>
2025-03-27 20:01:43 +0800

    Renamed device tree and optimized first boot user setup

--- .github/scripts/configs/alpine-startup.sh
+++ .github/scripts/configs/alpine-startup.sh
@@ -5,6 +5,13 @@ echo "[$(date '+%Y-%m-%d %H:%M:%S')] Hello from Alpine Linux!"
 # set led trigger to none
 echo "none" > /sys/class/leds/blue\:wan/trigger
 
+# set hostname, hostname from /proc/device-tree/compatible
+USER_NAME=$(awk -F',' 'NR==1 {print $2}' /proc/device-tree/compatible)
+USER_PASSWD="admin${USER_NAME}"
+setup-hostname ${USER_NAME}
+setup-user -u ${USER_NAME}
+echo "${USER_NAME}:${USER_PASSWD}" | chpasswd
+
 # expand rootfs
 resize2fs /dev/disk/by-partlabel/system
 
